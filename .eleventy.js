6febca5e14b5c100e58103e96383f2e54c334510
Saahil Jaffer <saahiljaffer@ritual.co>
2022-03-15 17:13:48 -0400

    Adds alpine.js and tailwindcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+const now = String(Date.now());
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./tailwind.config.js");
+  eleventyConfig.addWatchTarget("./src/css/tailwind.css");
+
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/cdn.js": "./js/alpine.js",
+  });
+
+  eleventyConfig.addShortcode("version", function () {
+    return now;
+  });
+
   eleventyConfig.addPassthroughCopy("./src/event.ics");
 
   return {
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["public/**/*.html"],
+  safelist: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
