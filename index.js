96f87fda3642eb60cc95c338241ceb11f4616068
Saahil Jaffer <saahiljaffer@ritual.co>
2022-03-15 16:25:02 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const ics = require("ics");
+const { writeFileSync } = require("fs");
+const momentHijri = require("moment-hijri");
+const events = require("./events.json");
+
+const years = ["1443", "1444"];
+let myEventsList = [];
+
+for (let i = 0; i < years.length; i++) {
+  myEventsList = myEventsList.concat(
+    events.map((event) => {
+      let startDate = momentHijri(`${years[i]}/${event.date}`, "iYYYY/iM/iD");
+      if (!startDate.isValid()) {
+        startDate = momentHijri(
+          `${years[i]}/${event.fallbackDate}`,
+          "iYYYY/iM/iD"
+        );
+      }
+      let endDate = startDate.clone().add(1, "d");
+      return {
+        start: [startDate.year(), startDate.month() + 1, startDate.date()],
+        end: [endDate.year(), endDate.month() + 1, endDate.date()],
+        title: event.title,
+      };
+    })
+  );
+}
+
+ics.createEvents(myEventsList, (error, value) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  writeFileSync(`${__dirname}/event.ics`, value);
+});
