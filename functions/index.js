a79e97fab5340493e7a62b0a0961213c4f634af2
Saahil Jaffer <contact@saahiljaffer.com>
2024-03-15 13:11:23 -0400

    Refactor prayer time formatting and remove unnecessary Firebase initialization

--- functions/index.js
+++ functions/index.js
@@ -3,29 +3,24 @@ const moment = require("moment");
 const momentTimezone = require("moment-timezone");
 const momentHijri = require("moment-hijri");
 const momentHijriTimezone = require("hijri-moment-timezone");
-const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
 exports.times = functions
   .region("us-east1")
   .https.onRequest(async (req, res) => {
-    initializeApp();
     const db = getFirestore();
     const today = moment().locale("en").tz("America/Toronto");
     const doc = await db.collection("times").doc(today.format("MM-DD")).get();
-    const data = doc.data();
-    const modifiedPrayers = Object.keys(data).map((name) => {
-      return {
-        time: moment(data[name], "h:mm")
-          .locale("en")
-          .add(today.isDST(), "hours")
-          .format("h:mm"),
-        name,
-      };
+    const prayers = doc.data();
+    Object.keys(prayers).forEach((name) => {
+      prayers[name] = moment(prayers[name], "h:mm")
+        .locale("en")
+        .add(today.isDST(), "hours")
+        .format("h:mm");
     });
     const islamicDate = momentHijri().locale("en").tz("America/Toronto");
     res.json({
-      prayers: modifiedPrayers,
+      prayers,
       day: islamicDate.format("iDo"),
       month: islamicDate.format("iMMMM"),
       year: islamicDate.format("iYYYY"),
